1ab774dfeab3c2cda76edf87901479373a2016a0
razzak <razzaksr@gmail.com>
2019-04-29 14:53:09 +0530

    added new folder Prabhu to firstdivison

---
+++ Prabhu/BasicProgram.java
@@ -0,0 +1,19 @@
+class basicProgram
+{
+public static void main(String[] jack)
+{
+System.out.println("Welcome to Zealous");
+Character dataOne='a', dataTwo='A';
+System.out.println(dataOne+dataTwo);
+System.out.println(dataTwo/4);
+System.out.println(dataTwo%4);
+}
+}
+
+/*
+void main()
+{
+printf()
+}
+cout
+*/
